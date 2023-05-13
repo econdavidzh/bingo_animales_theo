47a5e469877adcf7b20b2ab180df7aa709ebf4b1
David Zambrano <82787004+econdavidzh@users.noreply.github.com>
2023-05-13 05:22:50 -0500

    Update Theo.py

--- Theo.py
+++ Theo.py
@@ -7,18 +7,6 @@ def obtener_archivos_en_carpeta(ruta_carpeta):
     archivos = glob.glob(os.path.join(ruta_carpeta, '*'))
     return archivos
 
-def obtener_elemento_aleatorio(lista):
-    if len(lista) == 0:
-        return None
-    
-    # Seleccionar un elemento aleatorio de la lista
-    elemento_seleccionado = random.choice(lista)
-    
-    # Eliminar el elemento seleccionado de la lista
-    lista.remove(elemento_seleccionado)
-    
-    return elemento_seleccionado
-
 # Ruta de la carpeta que deseas explorar
 lista_animales = obtener_archivos_en_carpeta("Animales")
 
@@ -26,6 +14,18 @@ st.title("Bingo Theo!")
         
 # Botón para mostrar una imagen aleatoria
 if st.button("Selección aleatoria de un animalito :D"):
+    
+    def obtener_elemento_aleatorio(lista):
+    if len(lista) == 0:
+        return None
+    
+        # Seleccionar un elemento aleatorio de la lista
+        elemento_seleccionado = random.choice(lista)
+    
+        # Eliminar el elemento seleccionado de la lista
+        lista.remove(elemento_seleccionado)
+    
+        return elemento_seleccionado
         
     # Obtener una imagen aleatoria
     imagen_aleatoria = obtener_elemento_aleatorio(lista_animales)
