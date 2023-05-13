43bb020f4831bc534f618dca59e0bd71e5ee83bc
David Zambrano <82787004+econdavidzh@users.noreply.github.com>
2023-05-12 22:53:33 -0500

    Update Theo.py

--- Theo.py
+++ Theo.py
@@ -7,22 +7,28 @@ def obtener_archivos_en_carpeta(ruta_carpeta):
     archivos = glob.glob(os.path.join(ruta_carpeta, '*'))
     return archivos
 
+def obtener_elemento_aleatorio(lista):
+    if len(lista) == 0:
+        return None
+    
+    # Seleccionar un elemento aleatorio de la lista
+    elemento_seleccionado = random.choice(lista)
+    
+    # Eliminar el elemento seleccionado de la lista
+    lista.remove(elemento_seleccionado)
+    
+    return elemento_seleccionado
+
 # Ruta de la carpeta que deseas explorar
-ruta_carpeta = "Animales"
+lista_animales = obtener_archivos_en_carpeta("Animales")
 
 st.title("Bingo Theo!")
-    
-try:
-    lista_animales = lista_animales
-except:
-    lista_animales = obtener_archivos_en_carpeta(ruta_carpeta)
-    
+        
 # Botón para mostrar una imagen aleatoria
 if st.button("Selección aleatoria de un animalito :D"):
         
     # Obtener una imagen aleatoria
-    imagen_aleatoria = random.choice(lista_animales)
-    lista_animales = lista_animales.remove(imagen_aleatoria)
+    imagen_aleatoria = obtener_elemento_aleatorio(lista_animales)
         
     if imagen_aleatoria:
         # Mostrar la imagen
@@ -30,7 +36,7 @@ if st.button("Selección aleatoria de un animalito :D"):
             titulo = imagen_aleatoria.split('_')[1].split('.')[0]
             st.image(imagen_aleatoria)
             st.title('Letra: ' + imagen_aleatoria.split('_')[0].split('/')[1])
-            st.title(titulo)
+            st.title('Animal:' + titulo)
             
         except:
             st.write("Se acabaron los animalitos del bingo :(")
