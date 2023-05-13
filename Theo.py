0375ac6053b3db1594a34e4478942fb8e80e2d6c
David Zambrano <82787004+econdavidzh@users.noreply.github.com>
2023-05-13 05:44:24 -0500

    Update Theo.py

--- Theo.py
+++ Theo.py
@@ -4,6 +4,8 @@ import random
 import streamlit as st
 
 animales_seleccionados = []
+count = 0
+animals_dict = dict()
 
 def obtener_archivos_en_carpeta(ruta_carpeta):
     archivos = glob.glob(os.path.join(ruta_carpeta, '*'))
@@ -25,17 +27,21 @@ st.title("Bingo Theo!")
         
 # Botón para mostrar una imagen aleatoria
 if st.button("Selección aleatoria de un animalito :D"):
+    count += 1
+    lista_animales = [for x in lista_animales if x not in animales_seleccionados]
     # Obtener una imagen aleatoria
     imagen_aleatoria = obtener_elemento_aleatorio(lista_animales)
+    animals_dict[count] = imagen_aleatoria
+    
     if imagen_aleatoria:
         # Mostrar la imagen
         imagen_aleatoria = obtener_elemento_aleatorio(lista_animales)
         animales_seleccionados += [imagen_aleatoria]
-        lista_animales -= [imagen_aleatoria]
         try:
             titulo = imagen_aleatoria.split('_')[1].split('.')[0]
             st.image(imagen_aleatoria)
             st.title('Letra: ' + imagen_aleatoria.split('_')[0].split('/')[1] + ' - Animal: ' + titulo)
+            st.write(animals_dict)
             
         except:
             st.write("Se acabaron los animalitos del bingo :(")
