cb40b8d87ebcfdae95dc7d09ffa2d6453fc992a4
David Zambrano <82787004+econdavidzh@users.noreply.github.com>
2023-05-12 22:29:27 -0500

    Update Theo.py

--- Theo.py
+++ Theo.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import random
+import streamlit as st
 
 def obtener_archivos_en_carpeta(ruta_carpeta):
     archivos = glob.glob(os.path.join(ruta_carpeta, '*'))
@@ -9,9 +10,10 @@ def obtener_archivos_en_carpeta(ruta_carpeta):
 # Ruta de la carpeta que deseas explorar
 ruta_carpeta = "Animales"
 
-
-import streamlit as st
-
+try:
+    lista_animales = lista_animales
+except:
+    lista_animales = obtener_archivos_en_carpeta(ruta_carpeta)
 
 def main():
     st.title("Bingo Theo!")
@@ -19,13 +21,18 @@ def main():
     # Botón para mostrar una imagen aleatoria
     if st.button("Selección aleatoria de un animalito :D"):
         # Obtener una imagen aleatoria
-        lista_animales = obtener_archivos_en_carpeta(ruta_carpeta)
+        
         imagen_aleatoria = random.choice(lista_animales)
         
         if imagen_aleatoria:
             # Mostrar la imagen
             titulo = imagen_aleatoria.split('_')[1].split('.')[0]
-            st.image(imagen_aleatoria, caption=titulo)
+            st.image(imagen_aleatoria)
+            st.title('Letra: ' + imagen_aleatoria.split('_')[0])
+            st.title(titulo)
+            lista_animales.remove(imagen_aleatoria)
+            
+            
         else:
             st.write("No se encontraron imágenes en el directorio.")
     
